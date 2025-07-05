5c3f303b1148d34b8b9ca3d1d57708145c5f6865
hrjp <hrjp00@gmail.com>
2025-07-05 19:12:49 +0900

    fix: reorganize docker_run_ci.sh to create output directories before running the container

--- docker_run_ci.sh
+++ docker_run_ci.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #GITHUB_WORKSPACE=$(pwd)
-docker run --rm \
+mkdir -p output
+
+LOG_DIR="output/latest"
+mkdir -p $LOG_DIR
+docker run --rm -it \
   --name aichallenge-2025 \
   --network host \
   --privileged \
-  -v ${GITHUB_WORKSPACE}/output:/output \
   -v ${GITHUB_WORKSPACE}/aichallenge:/aichallenge \
-  -v ${GITHUB_WORKSPACE}/remote:/remote \
-  -v ${GITHUB_WORKSPACE}/vehicle:/vehicle \
   -e DISPLAY -e TERM \
   "aichallenge-2025-dev-${USER}" \
   bash -c "cd /aichallenge && ./build_autoware.bash && source /aichallenge/workspace/install/setup.bash && ./run_evaluation.bash"
